c61866f88968931316eab4a84dc6dd316ce99cb9
Brian Toh <brian.toh@live.com>
2018-01-19 10:13:12 +0800

    (feat) add task to automate dropping, creating, migrating and seeding of database

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Super Scripts
-> A bunch of scripts you can copy and paste into your command line to do the job for you ;)
+> A bunch of scripts you can run in your command line to do the job for you ;)
 > 
 > ## Usage
-> - Download/clone `ss.sh` file and place in root of project folder
+> - Download/clone/fork `ss.sh` file and place in root of project folder
 > - Click on `ss.sh` file and hit `cmd + i` to get information
-> - On the bottom right of the bar, click the lock icon to unlock. It will prompt for your computer's password
+> - On the bottom right of the bar, click the lock icon to unlock. It will prompt you for your computer's password
 > - On the command line, navigate to the root of your project and punch in `chmod 700 ss.sh` to give the bash file permissions
+> - To run a task, use `./ss.sh {{ taskName }}` e.g. `./ss.sh css`
 
 ## Organise CSS directory
 This command performs the following for a Rails project:
@@ -15,3 +16,9 @@ This command performs the following for a Rails project:
 - Imports all the new files into `application.css.sass`
 *Command*
 `./ss.sh css`
+
+## Drop, create, migrate and seed your database
+This command performs the following:
+- `rails db:drop db:create db:migrate db:seed`
+*Command*
+`./ss.sh rdb`
--- ss.sh
+++ ss.sh
@@ -7,28 +7,33 @@ if [ $# -gt 0 ]; then
 	# Organise CSS components function
 	if [ "$1" == "css" ]; then
 		shift 1
-		echo "====================="
-		echo "Creating styles folder in stylesheets directory"
-		echo "====================="
+		echo "*** Creating styles folder in stylesheets directory ***"
 		mkdir app/assets/stylesheets/styles
-		echo "====================="
-		echo "Generating sass files"
-		echo "====================="
+		echo "*** Generating sass files ***"
 		touch app/assets/stylesheets/styles/ _00_fontface.sass app/assets/stylesheets/styles/_00_variables.sass app/assets/stylesheets/styles/_10_layout.sass app/assets/stylesheets/styles/_10_typography.sass app/assets/stylesheets/styles/_20_animations.sass app/assets/stylesheets/styles/_20_buttons.sass app/assets/stylesheets/styles/_20_icons.sass app/assets/stylesheets/styles/_20_links.sass app/assets/stylesheets/styles/_30_footer.sass app/assets/stylesheets/styles/_30_nav.sass app/assets/stylesheets/styles/_40_home.sass
-		echo "====================="
-		echo "Converting application.css file to a sass file"
-		echo "====================="
+		echo "*** Converting application.css file to a sass file ***"
 		mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.sass
-		echo "====================="
-		echo "Import newly generated sass files into application.css.sass"
-		echo "====================="
+		echo "*** Import newly generated sass files into application.css.sass ***"
 		printf "\n@import './styles/*'" >> app/assets/stylesheets/application.css.sass
-		echo "====================="
-		echo "All done, baby."
-		echo "====================="
+		echo "*** All done, baby. ***"
+
+	# Run rails db:drop db:create db:migrate db:seed
+	elif [ "$1" == "rdb" ]; then
+		shift 1
+		echo "*** Dropping the database ***"
+		rails db:drop
+		echo "*** Creating the database ***"
+		rails db:create
+		echo "*** Migrating the database ***"
+		rails db:migrate
+		echo "*** Seeding the database ***"
+		rails db:seed
+		echo "*** Oh yeah, we good. ***"
+	
 	else
-		echo "Not working"
+		echo "*** Not working ***"
 	fi
+
 
 else
 	echo "No function selected"
